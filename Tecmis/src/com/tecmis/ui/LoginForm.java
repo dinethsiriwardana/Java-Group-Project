8157685f0af7033907e33263a9d7edb76412d017
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-06 09:01:05 +0530

    Database

--- Tecmis/src/com/tecmis/ui/LoginForm.java
+++ Tecmis/src/com/tecmis/ui/LoginForm.java
@@ -5,4 +5,7 @@ public class LoginForm extends JFrame {
     private JPanel pnlLogin;
     private JTextField textField1;
     private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton resetButton;
+    private JButton loginButton1;
 }
