d3e315f7680458fef49d6d10b6ab329ff47075cd
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-07 09:18:56 +0530

    Admin UI

---
+++ Tecmis/src/com/tecmis/ui/LectureForm.java
@@ -0,0 +1,12 @@
+package com.tecmis.ui;
+
+import javax.swing.*;
+
+public class LectureForm {
+    private JList list1;
+    private JButton manageMaterialsButton;
+    private JButton studentDetailsButton;
+    private JButton studentEligibilityButton;
+    private JButton studentResultButton;
+    private JButton medicalRecordButton;
+}
