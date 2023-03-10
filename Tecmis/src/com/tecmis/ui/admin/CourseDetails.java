bd2f57a2193a4f352fb7dcdd468dbdd6e79aa6f9
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2023-03-10 10:20:27 +0530

    EditCourseDetails
    Co-Authored-By: Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>

--- Tecmis/src/com/tecmis/ui/admin/CourseDetails.java
+++ Tecmis/src/com/tecmis/ui/admin/CourseDetails.java
@@ -1,32 +1,33 @@
 package com.tecmis.ui.admin;
 
+import javax.imageio.stream.ImageInputStream;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CourseDetails  extends JFrame {
+public class CourseDetails extends JFrame {
+
     private JTextField textID;
-    private JTextField textFirstName;
-    private JTextField textLastName;
-    private JTextField textMobile;
+    private JTextField textCourseName;
+    private JTextField textCredit;
+    private JTextField textCourseDepID;
     private JTable table1;
     private JButton ADDButton;
     private JButton DELETEButton;
     private JButton UPDATEButton;
-    private JTextField textLecturerId;
-    private JPanel pnlCourse;
     private JPanel panelCourse;
+    private JTextField texCourseLecID;
+
 
     public CourseDetails(){
-
         add(panelCourse);
         setVisible(true);
-        setTitle("View Course Details");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Course Details...");
         setSize(1000,600);
         setPreferredSize(new Dimension(220,400));
         setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
         ADDButton.addActionListener(new ActionListener() {
@@ -37,10 +38,8 @@ public class CourseDetails  extends JFrame {
         });
     }
 
-
-
     public static void main(String[] args) {
         CourseDetails coursedetail=new CourseDetails();
-
     }
+
 }
