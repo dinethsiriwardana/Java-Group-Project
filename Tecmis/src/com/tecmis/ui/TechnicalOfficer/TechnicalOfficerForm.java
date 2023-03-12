b5a7acdd8b00e7c0a56f52c920651510f10ad2ed
Anusarani Gunawardana <102348904+anusaranigunawardana@users.noreply.github.com>
2023-03-12 18:19:01 +0530

    12.03.2023.6.18.pm
    Co-Authored-By: Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>

--- Tecmis/src/com/tecmis/ui/TechnicalOfficer/TechnicalOfficerForm.java
+++ Tecmis/src/com/tecmis/ui/TechnicalOfficer/TechnicalOfficerForm.java
@@ -8,8 +8,14 @@ public class TechnicalOfficerForm extends JFrame {
     private JTextField textField2;
     private JTextField textField3;
     private JPanel PanalTechnicalOfficer;
-    private JButton submitButton;
+    private JButton updateButton;
     private JButton cancelButton;
+    private JTextField textField4;
+    private JTextField textField6;
+    private JTextArea textArea1;
+    private JTextField textField5;
+    private JTextField textField7;
+    private JComboBox comboBox1;
 
     public TechnicalOfficerForm(){
         add(PanalTechnicalOfficer);
