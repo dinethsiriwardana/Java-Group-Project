ca3542cd8a46a350ee2c48594095d9799168232c
DanukiAmaya2000 <123008398+DanukiAmaya2000@users.noreply.github.com>
2023-03-13 15:07:49 +0530

    student ui changes

--- Tecmis/src/com/tecmis/database/Database.java
+++ Tecmis/src/com/tecmis/database/Database.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class Database {
     private static Connection conn;
 
+    private static Connection connection;
 
 
     public static Connection getConnection() {
@@ -35,9 +36,14 @@ public class Database {
         }
     }
 
-//    public static void main(String[] args) {
-//        getConnection();
-//    }
+    public static Connection getDatabaseConnection() throws Exception{
+        // Load the MySQL JDBC driver
+        Class.forName("org.mariadb.jdbc.Driver");
+        // Create a connection to the database
+        connection = DriverManager.getConnection(
+                "jdbc:mariadb://191.96.56.1:3306/u812963415_javag2", "u812963415_javag2", "qEc:0f=5");
+        return connection;
+    }
 
 
 }
