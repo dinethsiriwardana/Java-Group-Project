b1cc6524c3f3aa3dba05ec93c4728e685c790d0e
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-13 11:21:23 +0530

    Add AdminData , StudentData , TechnicalOfficer

---
+++ Tecmis/src/com/tecmis/dto/StudentData.java
@@ -0,0 +1,114 @@
+package com.tecmis.dto;
+
+public class StudentData {
+
+    private String username;
+    private String password;
+    private String ID;
+    private String Fname;
+    private String Lname;
+    private String Mobile;
+    private String Address;
+    private String Age;
+    private String Email;
+    private String DOM;
+    private String Gender;
+    private String Level;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(String Mobile) {
+        this.Mobile = Mobile;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getDOM() {
+        return DOM;
+    }
+
+    public void setDOM(String DOM) {
+        this.DOM = DOM;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+}
