21abb31286d15ce7dde2f44be220b7ed18af7718
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2023-03-15 22:11:28 +0530

    UpdateAdminForm
    Co-Authored-By: Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>

--- Tecmis/src/com/tecmis/ui/admin/AdminForm.java
+++ Tecmis/src/com/tecmis/ui/admin/AdminForm.java
@@ -1,50 +1,38 @@
 package com.tecmis.ui.admin;
 
-import com.tecmis.database.Notices;
-import com.tecmis.ui.lecture.LectureForm;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-public class AdminForm extends JFrame{
+public class AdminForm extends  JFrame
 
-
-    private JButton btnAddUser;
-    private JButton btnAddLecturer ;
-    private JButton btnNoticeDetails;
-    private JButton btnAddTechnicalOfficer;
-    private JButton btnAddStudent;
+{
     private JPanel pnlAdmin;
+    private JButton addAdminButton;
+    private JButton manageCourseButton;
+    private JButton addLecturerButton;
+    private JButton manageNoticeButton;
+    private JButton addTechnicalOfficerButton;
+    private JButton manageTimetableButton;
+    private JButton addStudentButton;
     private JLabel lblWelcomText;
-    private JButton btnAddAdmin;
-    private JButton btnCourseDetails;
-    private JButton timeTableButton;
-    private JLabel lblNotices;
+    private JLabel lblSetting;
 
-    private String username;
-    static AdminForm adminForm;
-
-    public AdminForm() {
-
+    public  AdminForm() {
         add(pnlAdmin);
+
         setVisible(true);
-        setTitle("Admin");
+
+        setTitle("Admin Form!!!");
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1000, 600);
+
+        setSize(1000,600);
+
         setPreferredSize(new Dimension(220, 400));
-        setResizable(true);
-
-
+        
     }
+
     public static void main(String[] args) {
         AdminForm adminForm=new AdminForm();
-
     }
-
-
-
 }
