dd129ab3f20dd8479dc7197e8b1ad645bdda4683
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2023-03-18 15:25:17 +0530

    timetable
    Co-Authored-By: Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>

--- Tecmis/src/com/tecmis/database/ManageTimetable.java
+++ Tecmis/src/com/tecmis/database/ManageTimetable.java
@@ -1,14 +1,25 @@
 package com.tecmis.database;
-import java.io.*;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
+
+import com.tecmis.database.Database;
+
+import javax.swing.*;
+import java.sql.*;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class ManageTimetable {
     private String id;
     private String departmentName;
     private String level;
     private byte pdf;
+
+    public byte getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(byte pdf) {
+        this.pdf = pdf;
+    }
 
     public String getId() {
         return id;
@@ -34,7 +45,48 @@ public class ManageTimetable {
         this.level = level;
     }
 
-    public static void addTimetable() {
+    private static final String[] time_table_columns = {"Timetable_ID", "DepartmentName", "Level", "PDF"};
 
+    public static DefaultTableModel showTimetable() throws Exception {
+        Connection conn = Database.getDatabaseConnection();
+        Statement stmt = conn.createStatement();
+
+        ResultSet rs = stmt.executeQuery("SELECT " + String.join(",", time_table_columns) + " FROM Timetable");
+
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        DefaultTableModel model = new DefaultTableModel(time_table_columns, 0);
+
+        while (rs.next()) {
+            Object[] row = new Object[columnCount];
+            for (int i = 1; i <= columnCount; i++) {
+                row[i - 1] = rs.getObject(i);
+            }
+            model.addRow(row);
+        }
+
+        conn.close();
+        stmt.close();
+        rs.close();
+
+        return model;
+    }
+
+    public static void addTimetable(ManageTimetable managetable) throws SQLException {
+        try {
+            Connection conn = Database.getDatabaseConnection();
+            PreparedStatement stmt = conn.prepareStatement(
+                    "INSERT INTO Timetable (TimetableID, DepartmentName, Level, PDF) VALUES (?, ?, ?, ?)");
+            stmt.setString(1, managetable.getId());
+            stmt.setString(2, managetable.getDepartmentName());
+            stmt.setString(3, managetable.getLevel());
+            stmt.setByte(4, managetable.getPdf());
+            stmt.executeUpdate(); // execute the update statement to insert the data
+            conn.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- Tecmis/src/com/tecmis/ui/admin/Timetable.java
+++ Tecmis/src/com/tecmis/ui/admin/Timetable.java
@@ -1,16 +1,22 @@
 package com.tecmis.ui.admin;
 
+
+
+import com.tecmis.database.Course;
 import com.tecmis.database.ManageTimetable;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
 
 
 public class Timetable extends JFrame {
     private JPanel pnlUser;
     private JTextField txtTime_ID;
-    private JTable table1;
     private JButton addButton;
     private JButton deleteButton;
     private JButton updateButton;
@@ -18,23 +24,57 @@ public class Timetable extends JFrame {
     private JComboBox txtLevel;
     private JButton uploadButton;
     private JComboBox txtDepartment;
+    private JTable timeTable;
+    private JTextField txtPDF;
 
     public Timetable(){
         add(pnlTimetable);
         setVisible(true);
         setSize(1000,600);
         setTitle("Timetable!!!");
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        ManageTimetable managetables=new ManageTimetable();
+        try {
+            timeTable.setModel(managetables.showTimetable());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ManageTimetable managetable = new ManageTimetable();
+                managetable.setId(txtTime_ID.getText());
+                managetable.setDepartmentName(txtDepartment.getSelectedItem().toString());
+                managetable.setLevel(txtLevel.getSelectedItem().toString());
 
-                ManageTimetable.addTimetable();
+                // read the selected file and set the bytes to the PDF property of the managetable object
+                try {
+                    File pdfFile = new File(txtPDF.getText());
+                    byte[] pdfData= Files.readAllBytes(pdfFile.toPath());
+                    managetable.setPdf(pdfData[0]);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Failed to read PDF file: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                try {
+                    ManageTimetable.addTimetable(managetable);
+                    timeTable.setModel(managetable.showTimetable());
+                    JOptionPane.showMessageDialog(null, "Timetable added successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Failed to add timetable: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
+
+
+
+
         updateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
