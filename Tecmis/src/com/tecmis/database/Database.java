c96f8ece551aa2d0d7050602dac304e80d8ab484
Anusarani Gunawardana <102348904+anusaranigunawardana@users.noreply.github.com>
2023-03-19 19:43:45 +0530

    Final Edit
    Co-Authored-By: Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>

--- Tecmis/src/com/tecmis/database/Database.java
+++ Tecmis/src/com/tecmis/database/Database.java
@@ -11,22 +11,7 @@ public class Database {
         getDatabaseConnection();
     }
 
-    public static Connection getConnection() {
-        if (conn == null) {
-            try {
-                // Load the MySQL JDBC driver
-                Class.forName("org.mariadb.jdbc.Driver");
-                // Create a connection to the database
-                conn = DriverManager.getConnection(
 
-                "jdbc:mariadb://191.96.56.1:3306/u812963415_javag2", "u812963415_javag2", "qEc:0f=5");
-
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return conn;
-    }
 
     public static void closeConnection() {
         if (conn != null) {
@@ -50,19 +35,4 @@ public class Database {
 
 
 
-    public static void closeConnection() {
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            conn = null;
-        }
-    }
-
        }
-
-
-
-}
