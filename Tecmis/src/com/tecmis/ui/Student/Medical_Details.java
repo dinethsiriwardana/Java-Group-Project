0a9d5aef314cadd0644add44b6bb2b71b4cd3ab0
DanukiAmaya2000 <123008398+DanukiAmaya2000@users.noreply.github.com>
2023-05-27 18:31:09 +0530

    up

--- Tecmis/src/com/tecmis/ui/Student/Medical_Details.java
+++ Tecmis/src/com/tecmis/ui/Student/Medical_Details.java
@@ -2,6 +2,8 @@ package com.tecmis.ui.Student;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Medical_Details extends JFrame{
     private JLabel lblStuID;
@@ -28,7 +30,25 @@ public class Medical_Details extends JFrame{
     setPreferredSize(new Dimension(220,400));
     setResizable(true);
 
-}
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String studentID = textStuID.getText();
+                String courseID = textCourseID.getText();
+                String date = textDate.getText();
+                String selectedType = (String) TypecomboBox.getSelectedItem();
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textStuID.setText("");
+                textCourseID.setText("");
+                textDate.setText("");
+                TypecomboBox.setSelectedIndex(0);
+            }
+        });
+    }
 }
 
 
