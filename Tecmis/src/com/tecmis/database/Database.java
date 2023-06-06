0577f52a723fa38d05c8c62ace2d9d921cf008ea
DanukiAmaya2000 <123008398+DanukiAmaya2000@users.noreply.github.com>
2023-06-06 16:14:21 +0530

    up

--- Tecmis/src/com/tecmis/database/Database.java
+++ Tecmis/src/com/tecmis/database/Database.java
@@ -8,11 +8,24 @@ public class Database {
 
 
     public static Connection getDatabaseConnection() throws Exception {
+<<<<<<< Updated upstream
         // Load the MySQL JDBC driver
         Class.forName("org.mariadb.jdbc.Driver");
         // Create a connection to the database
         conn = DriverManager.getConnection(
                 "jdbc:mariadb://191.96.56.1:3306/u812963415_javag2", "u812963415_javag2", "qEc:0f=5");
+=======
+          // Load the MySQL JDBC driver
+//        Class.forName("org.mariadb.jdbc.Driver");
+//        // Create a connection to the database
+//        conn = DriverManager.getConnection(
+//                "jdbc:mariadb://191.96.56.1:3306/u812963415_javag2", "u812963415_javag2", "qEc:0f=5");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/u812963415_javag2","root","Danuki@123");
+
+>>>>>>> Stashed changes
         return conn;
     }
 
